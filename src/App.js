feb922a2d2873b6992965907c72fbfdb665cb5fa
Mika-eii <michaellaakimana0@gmail.com>
2023-02-08 11:32:58 -0800

    created list of shows

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
+import { getByDisplayValue } from '@testing-library/react';
 import './App.css';
 import showsData from  './shows-data'
+
+
+
+function Show(props) {
+return (
+  <div className='Show'>
+    <img className='Show-image' src= {props.showObject.image.medium} />
+    <h2>{props.showObject.name}</h2>
+  </div>
+)
+}
 
 function App() {
   return (
     <div className="App">
-      <h1>The Best TV Shows App</h1>
+      <h1>The Best TV Shows App by me</h1>
+      {showsData.map( item => <Show showObject={item}/> )}
     </div>
   );
 }
 
 export default App;
+
